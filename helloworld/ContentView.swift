502d39cc922a3122aa0c0f38a031c2d5c8a105e7
Lemon <phamchauanh1204@gmail.com>
2026-03-01 10:13:57 +0700

    Initial Commit

---
+++ helloworld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by thuhuong2025 on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by thuhuong2025 on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
